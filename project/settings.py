01218a7c486d013620fcb7acc00993a2ff8e6101
pavan-atchutha <20pa1a5404@vishnu.edu.in>
2023-03-11 11:07:00 +0530

    deploy debug

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = str(os.getenv('SECRET_KEY'))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
